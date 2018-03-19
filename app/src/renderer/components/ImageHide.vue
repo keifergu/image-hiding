d2e3dc644c3e10027e73f15a6e0618ac4f8631e3
Keifer Gu <keifergu@gmail.com>
2018-03-19 09:55:32 +0800

    feat(element): 部分美化样式

--- app/src/renderer/components/ImageHide.vue
+++ app/src/renderer/components/ImageHide.vue
@@ -1,31 +1,54 @@
 <template>
-  <div>
-    <input type="file" name="image" ref="image" @change="fileHandle">
-    <img :src="imageSrc" alt="">
-    <br> 加密字符串：<input type="text" v-model="str"><br>
-    加密方法：
-    <select name="method" v-model="method" >
-      <option value="header">头文件冗余加密</option>
-      <option value="data">数据区改写</option>
-      <option value="tail">图像尾部追加</option>
-    </select>
-    <br>
-    <button @click="generater">生成</button>
-    <br>
-    <a :href="outputPath">下载</a>
-    <br>
-    解密文件:
-    <input type="file" name="decode" ref="decode" @change="decodeFileHandle">
-    <br>
-    解密方法：
-    <select name="method" v-model="de_method" >
-      <option value="header">头文件冗余加密</option>
-      <option value="data">数据区改写</option>
-      <option value="tail">图像尾部追加</option>
-    </select>
-    <br>
-    <button @click="decode">解密</button>
-    {{decodeStr}}
+  <div class="con">
+    <h1 style="text-align:center;">数据图像隐藏</h1>
+    <el-upload
+      class="avatar-uploader"
+      action="#"
+      :show-file-list="false"
+      :on-change="fileHandle">
+      <img v-if="imageSrc" :src="imageSrc" class="avatar">
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+    <div class="de-str">
+      <span>加密字符串：</span>
+      <div style="width: 200px;"><el-input v-model="str" placeholder="请输入加密内容"></el-input></div>
+    </div>
+    <div class="de_method">
+      <span>加密方法：</span>
+      <el-select v-model="method" placeholder="请选择加密方法">
+        <el-option value="header" label="头文件冗余加密" />
+        <el-option value="data" label="数据区改写" />
+        <el-option value="tail" label="图像尾部追加" />
+      </el-select>
+    </div>
+    <div>
+      <el-button type="primary" @click="generater">生成加密图像</el-button>
+      <a :href="outputPath">下载加密图像</a>
+    </div>
+    <hr>
+    <div>
+      <div>解密文件:</div>
+      <div>
+        <input type="file" name="decode" ref="decode" @change="decodeFileHandle">
+      </div>
+    </div>
+    <div>
+      <div>
+        解密方法：
+      </div>
+      <div>
+        <el-select v-model="de_method" placeholder="请选择加密方法">
+          <el-option value="header" label="头文件冗余加密" />
+          <el-option value="data" label="数据区改写" />
+          <el-option value="tail" label="图像尾部追加" />
+        </el-select>
+      </div>
+    </div>
+    <div>
+      <el-button type="primary" @click="decode">解密图像</el-button>
+    </div>
+    <div>解密结果:</div>
+    <div>{{decodeStr}}</div>
   </div>
 </template>
 <script>
@@ -48,8 +71,8 @@ export default {
   mounted () {
   },
   methods: {
-    fileHandle () {
-      const file = this.$refs.image.files[0];
+    fileHandle (fileObj) {
+      const file = fileObj.raw;
       const imageSrc = `./static/${file.name}`;
       fs.readFile(file.path, (err, res) => {
         fs.writeFile(this.imageSrc, res, () => {
@@ -67,8 +90,54 @@ export default {
     },
     generater () {
       const { imageSrc, str, method, outputPath } = this;
-      hide.encode(imageSrc, str, 'data', outputPath)
+      hide.encode(imageSrc, str, 'data', outputPath);
+      this.$message('加密成功，请点击下载');
     }
   }
 }
 </script>
+
+<style>
+  .con {
+    margin: 20px;
+  }
+
+  div {
+    margin-top: 5px;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+
+  .de-str {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  .de-method {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+</style>
+
--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
+
+Vue.use(ElementUI);
+
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
